73a4d1bae24bf2f4049e078e110a90d6419563d9
Undal <krisunda@stud.ntnu.no>
2026-01-14 09:50:27 +0100

    start kalibrering

---
+++ lib/startSequence/startSequence.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <Zumo32U4.h>
+#include "startSequence.h"
+
+
+/*extren*/ Zumo32U4LineSensors lineSensors;
+/*extren*/ Zumo32U4Motors motors;
+
+void initSensors(){
+    lineSensors.initFiveSensors();
+}
+
+void spinZumo(){
+
+}
+
+void trackLinePosition(){
+
+}
+
+void stopOnLine(){
+    
+}
---
+++ lib/startSequence/startSequence.h
@@ -0,0 +1,8 @@
+#ifndef _START_SEQUENCE_
+#define _START_SEQUENCE_
+
+
+
+
+
+#endif
