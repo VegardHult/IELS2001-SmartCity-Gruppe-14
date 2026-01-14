cb29f59447a3c6fcba387d1972ff15fc5f420888
VegardHult <vegardhult@gmail.com>
2026-01-14 12:41:06 +0100

    Pulled utility file from gridNavigation

--- lib/utility/utility.cpp
+++ lib/utility/utility.cpp
@@ -1,1 +1,12 @@
 #include "utility.h"
+
+// Read encoder values
+intPair readEncoders() {
+    
+    intPair readClicks{};
+
+    readClicks.int1 = encoders.getCountsAndResetLeft();
+    readClicks.int2 = encoders.getCountsAndResetRight();
+
+    return readClicks;
+}
--- lib/utility/utility.h
+++ lib/utility/utility.h
@@ -1,6 +1,32 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 // Your declarations here
+#include <Arduino.h>
+#include <Zumo32U4.h>
+
+extern Zumo32U4Encoders encoders;
+
+enum nextAction {
+    S, // Straight
+    T, // Turn 180 degrees
+    L, // Turn left
+    R  // Turn right
+};
+
+enum modes {
+    PATROL,
+    EMERGENCY
+};
+
+extern modes mode;
+
+struct intPair {
+    int int1;
+    int int2;
+};
+
+// Read encoder values
+intPair readEncoders();
 
 #endif // UTILITY_H
 
