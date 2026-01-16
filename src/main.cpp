37981ef042e1cd7ae77017295e37d227f77dbf3b
VegardHult <vegardhult@gmail.com>
2026-01-16 14:07:54 +0100

    test makeTurn in loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <Arduino.h>
+#include <Zumo32U4.h>
+
 #include "battery.h"
 #include "display.h"
 #include "locate.h"
@@ -6,14 +9,12 @@
 #include "startSequence.h"
 #include "utility.h"
 
-#include <Arduino.h>
-#include <Zumo32U4.h>
-
 // Setup ZUMO modules
 Zumo32U4Encoders encoders;
 Zumo32U4Motors motors;
 
 modes mode = PATROL;
+actions action = I;
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,4 +22,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  makeTurn(90, 50);
+  motors.setSpeeds(0, 0);
+  delay(1000);
 }
