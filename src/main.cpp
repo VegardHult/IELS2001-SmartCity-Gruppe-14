d010e6ed11ab13997663fc1f82f7b6326938ef8a
Mordi <brittonefiskaa@gmail.com>
2026-01-21 10:09:53 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,20 @@
 #include "navigation.h"
 #include "startSequence.h"
 #include "utility.h"
+#include "lineFollowing.h"
 
 
 
 void setup() {
+    calibrateSensors();
+    Serial.begin(9600);
 }
 
 void loop() {
+    followLine();
+    if (paKryss()) {
+        Serial.println("Kryss detected!");
+        delay(1000); // Pause for a second to avoid multiple detections
+    }
+    
 }
