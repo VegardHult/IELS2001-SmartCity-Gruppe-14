2fc6b38e2d4af2970cab07b9ea43009201ddad1e
VegardHult <vegardhult@gmail.com>
2026-03-11 11:11:38 +0100

    Added wire functionality to Arduino

--- lib/utility/utility.cpp
+++ lib/utility/utility.cpp
@@ -10,3 +10,21 @@ intPair readEncoders() {
 
     return readClicks;
 }
+
+String handleWire(int addr, String data) {
+    // Send data to ESP-32
+    Wire.beginTransmission(addr);
+    Wire.write(data);
+    Wire.endTransmission();
+    
+    // Request next action from ESP-32
+    Wire.requestFrom(addr, 2); // Request 2 bytes (one character long string)
+    String receivedData = ""; // Clear existing data
+    while (Wire.available()) {
+        char c = Wire.read();
+        if (c == '\0') break; // Stop reading at the terminator
+        receivedData += c;
+    }
+
+    return recievedData;
+}
