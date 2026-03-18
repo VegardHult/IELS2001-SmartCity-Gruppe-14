924a9c44f129054664aa631af1caa6db0425de4b
Mordi <brittonefiskaa@gmail.com>
2026-03-18 10:48:49 +0100

    display emergency

--- lib/display/display.cpp
+++ lib/display/display.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include <Zumo32U4.h>
 #include "display.h"
+#include "utility.h"
+
+static bool textVisible = false;
+static unsigned long lastBlink = 0;
+static const unsigned long blinkInterval = 400;
 
 
 void clearScreen() {
@@ -14,4 +19,25 @@ void writeToScreen(String str, int line) {
     
     // Print a batteryCPercentage to screen
     display.print(str + "              ");
+}
+void updateDisplay(modes currentMode) {
+    if (currentMode == E) {
+        if (millis() - lastBlink >= blinkInterval)
+        {
+            lastBlink = millis();
+            textVisible = !textVisible;
+
+            display.clear();
+
+            if (textVisible)
+            {
+                display.print("EMERGNCY");
+            }
+        }
+    } else {
+        display.clear();
+        display.print(modeToString(currentMode));
+        textVisible = true;
+        lastBlink = millis();
+    }
 }
